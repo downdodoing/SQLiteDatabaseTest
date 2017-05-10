ea4cb62cff86745247a9459e3cc7dca1fedd7d26
downdodoing <2278802841@qq.com>
2017-05-10 21:55:09 +0800

    new function

--- app/src/main/java/com/example/mvp/sqlitedatabasetest/Merge.java
+++ app/src/main/java/com/example/mvp/sqlitedatabasetest/Merge.java
@@ -1,7 +1,16 @@
 package com.example.mvp.sqlitedatabasetest;
 
 public class Merge {
+    private String name;
     public Merge() {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
