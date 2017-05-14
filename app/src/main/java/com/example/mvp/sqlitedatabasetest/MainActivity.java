08a45993f53066607d71bf36c7c16d274dcd58af
downdodoing <2278802841@qq.com>
2017-05-14 21:29:41 +0800

    change code

--- app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
+++ app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.mvp.sqlitedatabasetest;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 
@@ -59,6 +65,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void getContacts(View view) {
         Intent intent = new Intent(this, ContactsActivity.class);
+        startActivity(intent);
+    }
+
+    public void notification(View view) {
+        Intent intent = new Intent(this, ContactsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content tittle")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .build();
+        manager.notify(1, notification);
+    }
+
+    public void camara(View view) {
+        Intent intent = new Intent(this, TakePhotoActivity.class);
         startActivity(intent);
     }
 }
