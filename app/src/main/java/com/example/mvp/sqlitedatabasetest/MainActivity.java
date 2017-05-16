e5dff6ce274c8960d5de2e759f2e0869e2f2d39c
downdodoing <2278802841@qq.com>
2017-05-16 22:15:30 +0800

    new change code

--- app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
+++ app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
@@ -7,21 +7,17 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.mvp.sqlitedatabasetest.downloadsample.DownloadActivity;
 import com.example.mvp.sqlitedatabasetest.lbs.LocationActivity;
-
-import com.example.mvp.sqlitedatabasetest.downloadsample.DownloadActivity;
 
 import org.litepal.LitePal;
 import org.litepal.crud.DataSupport;
