f015fdf07720913d7f4dd3b54f1cca19863237f2
downdodoing <2278802841@qq.com>
2017-05-16 21:38:05 +0800

    change code

--- app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
+++ app/src/main/java/com/example/mvp/sqlitedatabasetest/MainActivity.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+<<<<<<< Updated upstream
 import android.widget.Toast;
+=======
+
+import com.example.mvp.sqlitedatabasetest.downloadsample.DownloadActivity;
+import com.example.mvp.sqlitedatabasetest.lbs.LocationActivity;
+>>>>>>> Stashed changes
 
 import org.litepal.LitePal;
 import org.litepal.tablemanager.Connector;
@@ -27,4 +33,63 @@ public class MainActivity extends AppCompatActivity {
         book.setPrice(16.5);
         book.save();
     }
+<<<<<<< Updated upstream
+=======
+
+    public void getData(View view) {
+        List<Book> books = DataSupport.findAll(Book.class);
+        for (Book book1 : books) {
+            Log.i("MainActivity", book1.getAuthor() + " " + book1.getPrice());
+        }
+    }
+
+    public void getContacts(View view) {
+        Intent intent = new Intent(this, ContactsActivity.class);
+        startActivity(intent);
+    }
+
+    public void notification(View view) {
+        Intent intent = new Intent(this, ContactsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content tittle")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .build();
+        manager.notify(1, notification);
+    }
+
+    public void camara(View view) {
+        Intent intent = new Intent(this, TakePhotoActivity.class);
+        startActivity(intent);
+    }
+
+    public void getIntelData(View view) {
+        Intent intent = new Intent(this, GetIntelDataActivity.class);
+        startActivity(intent);
+    }
+
+    public void toService(View view) {
+        Intent intent = new Intent(this, MyServiceActivity.class);
+        startActivity(intent);
+    }
+
+    public void download(View view) {
+        Intent intent = new Intent(this, DownloadActivity.class);
+        startActivity(intent);
+    }
+
+    public void location(View view) {
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
+>>>>>>> Stashed changes
 }
---
+++ app/src/main/java/com/example/mvp/sqlitedatabasetest/lbs/LocationActivity.java
@@ -0,0 +1,160 @@
+package com.example.mvp.sqlitedatabasetest.lbs;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.example.mvp.sqlitedatabasetest.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationActivity extends AppCompatActivity {
+    private TextView show_localtion;
+    private LocationClient mLocationClient;
+    private MapView mapView;
+    private BaiduMap baiduMap;
+
+    private boolean isFirstLocate = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        SDKInitializer.initialize(getApplicationContext());
+
+        setContentView(R.layout.activity_location);
+
+        show_localtion = (TextView) findViewById(R.id.show_localtion);
+        mapView = (MapView) findViewById(R.id.mapView);
+        baiduMap = mapView.getMap();
+
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if ((ContextCompat.checkSelfPermission(LocationActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(LocationActivity.this, permissions, 1);
+        } else {
+            requestLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (1 == requestCode) {
+            if (grantResults.length > 0) {
+                for (int result : grantResults) {
+                    if (result != PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(LocationActivity.this, "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
+                        finish();
+                        return;
+                    }
+                }
+                requestLocation();
+            }
+        }
+    }
+
+    private void requestLocation() {
+
+        LocationClientOption option = new LocationClientOption();
+        //设置更新的时间段
+        option.setScanSpan(5000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+    }
+
+    class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+//            StringBuilder currentPosition = new StringBuilder();
+//            currentPosition.append("纬度：").append(bdLocation.getLatitude()).append("\n");
+//            currentPosition.append("经线：").append(bdLocation.getLongitude()).append("\n");
+//            currentPosition.append("定位方式：");
+//            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {
+//                currentPosition.append("GPS");
+//            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+//                currentPosition.append("网络");
+//            }
+//            show_localtion.setText(currentPosition.toString());
+//            Log.i("TAG", "onReceiveLocation: " + currentPosition.toString());
+
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation || bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {
+                navigateTo(bdLocation);
+            }
+        }
+
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+            Log.i("TAG", "onConnectHotSpotMessage: " + s);
+        }
+    }
+
+    private void navigateTo(BDLocation bdLocation) {
+        if (isFirstLocate) {
+            //用于存储经度纬度
+            LatLng ll = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+            //可以将地图移动到指定的经度纬度上面
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            //设置缩放级别
+            update = MapStatusUpdateFactory.zoomBy(16f);
+            baiduMap.animateMapStatus(update);
+
+            isFirstLocate = false;
+        }
+        //让“我”显示在地图上
+        MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+        locationBuilder.latitude(bdLocation.getLatitude());
+        locationBuilder.longitude(bdLocation.getLongitude());
+        MyLocationData locationData = locationBuilder.build();
+        baiduMap.setMyLocationData(locationData);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        mLocationClient.stop();
+    }
+}
